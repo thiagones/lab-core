8db1969118d49cf6dccc34d1d295a699ea3b0978
Thiago Melkior de Toledo Borges <thiago.m.borges@kroton.com.br>
2019-11-23 23:41:39 -0300

    substituição nginx pelo kong, ajuste log e api de healthcheck

--- lab.webapi/Controllers/HealthCheckController.cs
+++ lab.webapi/Controllers/HealthCheckController.cs
@@ -1,17 +1,8 @@
-﻿using System.Threading.Tasks;
-using AutoMapper;
-using lab.domain.Models.Api;
-using lab.service.Interfaces;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
 using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using lab.domain.Models;
 using Microsoft.Extensions.Configuration;
 
 namespace lab.webapi.Controllers
@@ -20,6 +11,7 @@ namespace lab.webapi.Controllers
     [Authorize]
     [ApiVersion("1.0")]
     [ApiVersion("2.0")]
+    [Route("api/[controller]")]
     [Route("api/v{version:apiVersion}/[controller]")]
     public class HealthCheckController : ControllerBase
     {
@@ -42,12 +34,8 @@ namespace lab.webapi.Controllers
         [Route("auth")]
         public IActionResult GetAuthenticated()
         {
-            _logger.LogInformation($"Iniciando -> GetAuthenticated");
-
             var healthCheck = Environment.GetEnvironmentVariable("HEALTHCHECK") ?? "";
 
-            _logger.LogInformation($"HealthCheck -> {healthCheck}");
-            
             var id = Guid.NewGuid().ToString();
             var result = new
             {
@@ -57,9 +45,7 @@ namespace lab.webapi.Controllers
                 Now = DateTime.Now
             };
 
-            _logger.LogTrace($"Resultado -> {result}");
-
-            _logger.LogInformation($"Finalizando -> GetAuthenticated");
+            _logger.LogInformation($"Auth Get HealthCheck -> {healthCheck}");
 
             return new OkObjectResult(result);
         }
@@ -68,11 +54,7 @@ namespace lab.webapi.Controllers
         [AllowAnonymous]
         public IActionResult Get()
         {
-            _logger.LogInformation($"Iniciando -> Get");
-
             var healthCheck = Environment.GetEnvironmentVariable("HEALTHCHECK") ?? "";
-
-            _logger.LogInformation($"HealthCheck -> {healthCheck}");
 
             var id = Guid.NewGuid().ToString();
             var result = new
@@ -83,9 +65,7 @@ namespace lab.webapi.Controllers
                 Now = DateTime.Now
             };
 
-            _logger.LogTrace($"Resultado -> {result}");
-
-            _logger.LogInformation($"Finalizando -> Get");
+            _logger.LogInformation($"Get HealthCheck -> {healthCheck}");
 
             return new OkObjectResult(result);
         }
@@ -94,11 +74,7 @@ namespace lab.webapi.Controllers
         [Route("auth")]
         public IActionResult PostAuthenticated()
         {
-            _logger.LogInformation($"Iniciando -> PostAuthenticated");
-
             var healthCheck = Environment.GetEnvironmentVariable("HEALTHCHECK") ?? "";
-
-            _logger.LogInformation($"HealthCheck -> {healthCheck}");
 
             var id = Guid.NewGuid().ToString();
             var result = new
@@ -109,9 +85,7 @@ namespace lab.webapi.Controllers
                 Now = DateTime.Now
             };
 
-            _logger.LogTrace($"Resultado -> {result}");
-
-            _logger.LogInformation($"Finalizando -> PostAuthenticated");
+            _logger.LogInformation($"Auth Post HealthCheck -> {healthCheck}");
 
             return new CreatedResult(id, result);
         }
@@ -120,11 +94,7 @@ namespace lab.webapi.Controllers
         [AllowAnonymous]
         public IActionResult Post()
         {
-            _logger.LogInformation($"Iniciando -> Post");
-
-            var healthCheck = Environment.GetEnvironmentVariable("HEALTHCHECK");
-
-            _logger.LogInformation($"HealthCheck -> {healthCheck}");
+            var healthCheck = Environment.GetEnvironmentVariable("HEALTHCHECK") ?? "";
 
             var id = Guid.NewGuid().ToString();
             var result = new
@@ -135,9 +105,7 @@ namespace lab.webapi.Controllers
                 Now = DateTime.Now
             };
 
-            _logger.LogTrace($"Resultado -> {result}");
-
-            _logger.LogInformation($"Finalizando -> Post");
+            _logger.LogInformation($"Post HealthCheck -> {healthCheck}");
 
             return new CreatedResult(id, result);
         }
